c2b4931654561b49c9c113f1cbeebf1cf978e979
Sara <saraahmad1114@yahoo.com>
2016-10-06 13:45:25 -0400

    done with the trailer calling APIClient

--- MovieProjectApp/TheDBMovieAPIClient.swift
+++ MovieProjectApp/TheDBMovieAPIClient.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class DBMovieAPIClient
+class TheDBMovieAPIClient
 {
     
     class func getMovieTrailerFrom(query: String, completion: (String)->())
@@ -25,9 +25,34 @@ class DBMovieAPIClient
         
         request.HTTPMethod = "GET"
         
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) in
+            
+            guard let unwrappedData = data else {print("Error occurred here"); return}
+            
+            if let responseDictionary = try? NSJSONSerialization.JSONObjectWithData(unwrappedData, options: []) as? NSDictionary
+            {
+                guard let unwrappedResponseDictionary = responseDictionary else {print("ERROR"); return}
+                
+                let resultsArray = unwrappedResponseDictionary["results"] as? NSArray
+                
+                guard let unwrappedResultsArray = resultsArray else {print("ERROR"); return}
+                
+                let neededDictionary = unwrappedResultsArray[0] as? NSDictionary
+                
+                guard let unwrappedNeededDictionary = neededDictionary else {print("ERROR"); return}
+                
+                let neededKey = unwrappedNeededDictionary["key"] as? String
+                
+                guard let unwrappedNeededKey = neededKey else {print("ERROR"); return}
+                
+                videoKey = unwrappedNeededKey
+            
+            }
+            completion(videoKey)
+        }
         
-        
-        
+        task.resume()
+
     }
 
 }
