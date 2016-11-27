861897014fb98800771ea6738f7f1dc965c8d53c
Sara <saraahmad1114@yahoo.com>
2016-11-26 23:15:13 -0500

    added the movietrailer class

---
+++ MovieProjectApp/movieTrailer.swift
@@ -0,0 +1,20 @@
+//
+//  movieTrailer.swift
+//  MovieProjectApp
+//
+//  Created by Flatiron School on 11/26/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class movieTrailer {
+
+    var key: String?
+    
+    //designated initializer 
+    init(key: String){
+        self.key = key
+    }
+
+}
